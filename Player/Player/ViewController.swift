aecabb207ad661b8a1ab8c245695fc6f90a181ef
chooblarin <choo.bla.rin@gmail.com>
2017-05-04 00:20:16 +0900

    Slider demo

--- Player/Player/ViewController.swift
+++ Player/Player/ViewController.swift
@@ -2,9 +2,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  let slider: UISlider = {
+    let slider = UISlider()
+    slider.backgroundColor = .gray
+    return slider
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    view.backgroundColor = .white
+
+    view.addSubview(slider)
+    slider.translatesAutoresizingMaskIntoConstraints = false
+    slider.heightAnchor.constraint(equalToConstant: 24.0).isActive = true
+    slider.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    slider.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20.0).isActive = true
+    slider.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20.0).isActive = true
+
+    slider.addTarget(self, action: #selector(sliderChanged(_:)), for: .touchUpInside)
   }
 
   override func didReceiveMemoryWarning() {
@@ -12,5 +27,9 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  func sliderChanged(_ sender: UISlider) {
+    debugPrint("aaa")
+  }
 }
+
 
